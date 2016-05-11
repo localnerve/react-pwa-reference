fccc5eba81c338338cda334dde3e1ede9890e235
Alex Grant <alex@localnerve.com>
2016-05-11 16:19:28 -0400

    ensure symlink target dir exists

--- src/build/symlink.js
+++ src/build/symlink.js
@@ -3,9 +3,11 @@
  * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
  */
 import gulp from 'gulp';
+import { sync as mkdirp } from 'mkdirp'
 
 /**
  * Factory for the symlink task.
+ * Ensure the link target exists (allowEmpty on src will not work).
  *
  * @param {Object} settings - The project settings.
  * @param {Boolean} output - True to create symlink in output, false for src.
@@ -15,9 +17,9 @@ export default function symlinkTaskFactory (settings, output) {
   const target = output ? 'output' : 'src';
 
   return function symlink () {
-    return gulp.src(`./${settings[target].application}`, {
-      allowEmpty: true
-    })
+    mkdirp(`./${settings[target].application}`);
+
+    return gulp.src(`./${settings[target].application}`)
     .pipe(gulp.symlink(`./${settings[target].baseDir}/node_modules`));
   }
 }
