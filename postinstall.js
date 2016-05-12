050cb49b31cfe059cb457766016c0d24acc61f83
Alex Grant <alex@localnerve.com>
2016-05-12 17:19:15 -0400

    fix heroku deployment

---
+++ postinstall.js
@@ -0,0 +1,40 @@
+/***
+ * Copyright (c) 2016 Alex Grant (@localnerve), LocalNerve LLC
+ * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
+ *
+ * The universal postinstall script.
+ */
+/* global require, console */
+/*eslint-disable no-console */
+'use strict';
+
+const fs = require('fs'),
+  path = require('path');
+
+// try to setup the src symlink.
+try {
+  fs.symlinkSync(
+    path.resolve('./src/application'),
+    './src/node_modules/application',
+    'dir'
+  );
+  console.log('*** Successfully setup src application symlink ***');
+} catch(e){
+  console.error(
+    '*** FAILED to create src application symlink *** ' + e
+  );
+}
+
+// try to setup the output symlink.
+try {
+  fs.symlinkSync(
+    path.resolve('./output/application'),
+    './output/node_modules/application',
+    'dir'
+  );
+  console.log('*** Successfully setup output application symlink ***');
+} catch (e) {
+  console.error(
+    '*** FAILED to create src/node_modules/application symlink *** ' + e
+  );
+}
