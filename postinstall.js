489ba5dcb1184b604021447b03626b4a3ed3ee1b
Alex Grant <alex@localnerve.com>
2016-05-15 10:41:08 -0400

    cleanup postinstall

--- postinstall.js
+++ postinstall.js
@@ -10,30 +10,29 @@
 
 const fs = require('fs');
 
-// try to setup the src symlink.
-try {
-  fs.symlinkSync(
-    '../application',
-    './src/node_modules/application',
-    'dir'
-  );
-  console.log('*** Successfully setup src application symlink ***');
-} catch(e){
-  console.error(
-    '*** FAILED to create src symlink *** ' + e
-  );
+/**
+ * Create the appliction symlink, fail quietly if exists.
+ * Setting up 'src' is only required for local development/running.
+ * Setting up 'output' is only required for the remote host deployment.
+ *
+ * @param {String} type - one of 'src' or 'output'.
+ */
+function applicationSymLink (type) {
+  try {
+    fs.symlinkSync(
+      '../application',
+      `./${type}/node_modules/application`,
+      'dir'
+    );
+    console.log(`*** Successfully setup ${type} application symlink ***`);
+  } catch(e){
+    if (e.code !== 'EEXIST') {
+      console.error(
+        `*** FAILED to create ${type} symlink. This might be ok. ***
+        ${e}`
+      );
+    }
+  }
 }
 
-// try to setup the output symlink.
-try {
-  fs.symlinkSync(
-    '../application',
-    './output/node_modules/application',
-    'dir'
-  );
-  console.log('*** Successfully setup output application symlink ***');
-} catch (e) {
-  console.error(
-    '*** FAILED to create output symlink *** ' + e
-  );
-}
+['src', 'output'].forEach((type) => applicationSymLink(type));
