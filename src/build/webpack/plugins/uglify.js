0de8d324f1ff18a4053335be8cdcc6c2caf200b9
Alex Grant <alex@localnerve.com>
2018-09-19 13:55:26 -0400

    chore(package): uglify -> terser for es6 min options

--- src/build/webpack/plugins/uglify.js
+++ src/build/webpack/plugins/uglify.js
@@ -1,21 +1,18 @@
 /***
  * Copyright (c) 2016 - 2018 Alex Grant (@localnerve), LocalNerve LLC
  * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
+ * 
+ * Kept to allow custom minification options. Historically, was UglifyJS (thus name).
  */
 
-// NOTE:
-// According to https://webpack.js.org/plugins/uglifyjs-webpack-plugin/
-// UglifyJsPlugin alias on schedule to be restored webpack.optimize.
-// import webpack from 'webpack';
-
-import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
 
 /**
- * Create the webpack uglifyJSPlugin with its options.
+ * Create the webpack es6 compression with custom options.
  */
 export default function uglifyPluginFactory () {
-  return new UglifyJsPlugin({
-    uglifyOptions: {
+  return new TerserPlugin({
+    terserOptions: {
       compress: {
         warnings: false
       },
