b6803fb12d51a6d74f9328d4d471368e93694f67
Alex Grant <alex@localnerve.com>
2019-01-24 21:39:03 -0500

    Add aria labels to ribbon svg

--- src/application/components/header/Ribbon.jsx
+++ src/application/components/header/Ribbon.jsx
@@ -26,22 +26,26 @@ class Ribbon extends React.Component {
     return (
       <div className="grid-row-spaced ribbon">
         <NavLink className="glyph" routeName="contact">
-          <svg className="icon icon-envelop">
+          <svg className="icon icon-envelop" aria-labelledby="envelop-title" role="img">
+            <title id="envelop-title">Contact Us</title>
             <use xlinkHref="#icon-mail2"></use>
           </svg>
         </NavLink>
         <a className="glyph" href={uriTel}>
-          <svg className="icon icon-phone">
+          <svg className="icon icon-phone" aria-labelledby="phone-title" role="img">
+            <title id="phone-title">Call Us</title>
             <use xlinkHref="#icon-phone"></use>
           </svg>
         </a>
         <a className="glyph" href={this.props.social.twitter}>
-          <svg className="icon icon-twitter">
+          <svg className="icon icon-twitter" aria-labelledby="twitter-title" role="img">
+            <title id="twitter-title">Our Twitter</title>
             <use xlinkHref="#icon-twitter"></use>
           </svg>
         </a>
         <a className="glyph" href={this.props.social.github}>
-          <svg className="icon icon-github">
+          <svg className="icon icon-github" aria-labelledby="github-title" role="img">
+            <title id="github-title">Our Github</title>
             <use xlinkHref="#icon-github"></use>
           </svg>
         </a>
@@ -51,7 +55,8 @@ class Ribbon extends React.Component {
           // Only add this cost if client doesn't support sw
           hide: !serverRender && !this.props.hasServiceWorker
         })} data={this.props.settings}>
-          <svg className="icon icon-cog">
+          <svg className="icon icon-cog" aria-labelledby="settings-title" role="img">
+            <title id="settings-title">Application Settings</title>
             <use xlinkHref="#icon-cog"></use>
           </svg>
         </ModalLink>
