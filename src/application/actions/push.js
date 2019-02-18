e53d81087fe8d5f635feef12fb55dffdf683eb21
Alex Grant <alex@localnerve.com>
2019-02-18 10:31:44 -0500

    chore(package): eslint 5.14.0, fix errant lint rule - push action, close #1002

--- src/application/actions/push.js
+++ src/application/actions/push.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2016 - 2019 Alex Grant (@localnerve), LocalNerve LLC
  * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
  */
-/* global process, window */
+/* global window */
 import debugLib from 'debug';
 import { getSubscriptionId } from 'utils/push';
 import syncable from 'utils/syncable';
